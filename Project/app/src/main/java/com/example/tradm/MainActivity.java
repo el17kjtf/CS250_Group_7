ae44646d5c8200e5731eb888ef02048577a530b8
Mohamed Almaazmi <53411372+lemonadeeeee@users.noreply.github.com>
2019-12-02 22:50:25 +0400

    MarketPlace Initial Work

--- Project/app/src/main/java/com/example/tradm/MainActivity.java
+++ Project/app/src/main/java/com/example/tradm/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<ExampleItem> exampleList = new ArrayList<>();
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line1", "Line2"));
+        exampleList.add(new ExampleItem(R.drawable.ic_cake, "Line3", "Line4"));
+        exampleList.add(new ExampleItem(R.drawable.ic_whatshot, "Line5", "Line6"));
+
     }
 }
