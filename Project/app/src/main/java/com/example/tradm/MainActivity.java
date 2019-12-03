ef334765fd2a9b88e40638772c09c40ace61a36e
Mohamed Almaazmi <53411372+lemonadeeeee@users.noreply.github.com>
2019-12-03 20:51:57 +0400

    Market Fiddling

--- Project/app/src/main/java/com/example/tradm/MainActivity.java
+++ Project/app/src/main/java/com/example/tradm/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tradm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -8,15 +10,39 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ArrayList<ExampleItem> exampleList = new ArrayList<>();
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line1", "Line2"));
-        exampleList.add(new ExampleItem(R.drawable.ic_cake, "Line3", "Line4"));
-        exampleList.add(new ExampleItem(R.drawable.ic_whatshot, "Line5", "Line6"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 1", "Line 2"));
+        exampleList.add(new ExampleItem(R.drawable.ic_cake, "Line 3", "Line 4"));
+        exampleList.add(new ExampleItem(R.drawable.ic_whatshot, "Line 5", "Line 6"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 7", "Line 8"));
+        exampleList.add(new ExampleItem(R.drawable.ic_cake, "Line 9", "Line 10"));
+        exampleList.add(new ExampleItem(R.drawable.ic_whatshot, "Line 11", "Line 12"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 13", "Line 14"));
+        exampleList.add(new ExampleItem(R.drawable.ic_cake, "Line 15", "Line 16"));
+        exampleList.add(new ExampleItem(R.drawable.ic_whatshot, "Line 17", "Line 18"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 19", "Line 20"));
+        exampleList.add(new ExampleItem(R.drawable.ic_cake, "Line 21", "Line 22"));
+        exampleList.add(new ExampleItem(R.drawable.ic_whatshot, "Line 23", "Line 24"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 25", "Line 26"));
+        exampleList.add(new ExampleItem(R.drawable.ic_whatshot, "Line 27", "Line 28"));
+        exampleList.add(new ExampleItem(R.drawable.ic_cake, "Line 29", "Line 30"));
 
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(exampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
