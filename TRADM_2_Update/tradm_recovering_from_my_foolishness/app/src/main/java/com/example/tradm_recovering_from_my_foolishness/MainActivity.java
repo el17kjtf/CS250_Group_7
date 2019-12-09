791a9fbd81ad8947a07a75ad525a22c189548390
wulanfrom <biasindi.wulanfrom@gmail.com>
2019-12-10 06:42:16 +0900

    UI_Update
    Added the offer creation and home page update, for items in the home page, refer to item_bar.xml

---
+++ TRADM_2_Update/tradm_recovering_from_my_foolishness/app/src/main/java/com/example/tradm_recovering_from_my_foolishness/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tradm_recovering_from_my_foolishness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
