f344afea2ab9dfe4e4f295599d867c48e140259d
Qi-Zihan <415744189@qq.com>
2019-12-10 21:40:45 +0900

    service info and item info

---
+++ src/main/java/com/example/accountpage/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.accountpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
