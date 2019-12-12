46a4c9f90d603f931d77e9069e09403ac063b891
Mohamed Almaazmi <53411372+lemonadeeeee@users.noreply.github.com>
2019-12-12 17:40:06 +0400

    DON'T DOWNLOAD THIS COMMI

--- TRADM/app/src/main/java/com/example/tradm/Offer.java
+++ TRADM/app/src/main/java/com/example/tradm/Offer.java
@@ -4,7 +4,6 @@ public class Offer {
     private String title;
     private String description;
     private int sellerID;
-    private int buyerID;
     private OfferStat offerStatus;
     private String offerType;
     private int price;
@@ -27,51 +26,14 @@ public class Offer {
         this.upload = upload;
     }
 
-    public Offer(String title, String description, int buyerID, int sellerID, OfferStat offerStatus, String offerType, int price, Upload upload) {
+    public Offer(String title, String description, int sellerID, OfferStat offerStatus, String offerType, int price, Upload upload) {
         this.title = title;
         this.description = description;
-        this.buyerID = buyerID;
         this.sellerID = sellerID;
         this.offerStatus = offerStatus;
         this.offerType = offerType;
         this.price = price;
         this.upload = upload;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setSellerID(int sellerID) {
-        this.sellerID = sellerID;
-    }
-
-    public void setBuyerID(int buyerID) {
-        this.buyerID = buyerID;
-    }
-
-    public void setOfferStatus(OfferStat offerStatus) {
-        this.offerStatus = offerStatus;
-    }
-
-    public void setOfferType(String offerType) {
-        this.offerType = offerType;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setUpload(Upload upload) {
-        this.upload = upload;
-    }
-
-    public int getBuyerID() {
-        return buyerID;
     }
 
     public int getPrice() {
--- TRADM/app/src/main/java/com/example/tradm/Upload.java
+++ TRADM/app/src/main/java/com/example/tradm/Upload.java
@@ -1,14 +1,26 @@
 package com.example.tradm;
 
 public class Upload {
+    private String Name;
     private String ImageUrl;
 
     public Upload(){
         //empty for firebase
     }
 
-    public Upload(String imageUrl) {
+    public Upload(String name, String imageUrl) {
+        if(name.trim().equals(""))
+            name = "No Name";
+        Name = name;
         ImageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
     }
 
     public String getImageUrl() {
