3db3a080b900490261faffd91c7db114a0923eb5
Mohamed Almaazmi <53411372+lemonadeeeee@users.noreply.github.com>
2019-12-12 14:13:52 +0400

    Final Commit Maybe?

---
+++ TRADM/app/src/main/java/com/example/tradm/Member.java
@@ -0,0 +1,26 @@
+package com.example.tradm;
+
+public class Member {
+    private String ID;
+    private String Email;
+
+    public Member() {
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
+
