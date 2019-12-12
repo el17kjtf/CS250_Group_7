ecc21f776d1313a2f1e92b912429d529f87b81b9
Mohamed Almaazmi <53411372+lemonadeeeee@users.noreply.github.com>
2019-12-12 17:48:28 +0400

    Revert "D"
    This reverts commit 2fcd8e053e228f7960f1dfb82fbb714f04f9427f.

---
+++ TRADM/app/src/main/java/com/example/tradm/Home.java
@@ -0,0 +1,31 @@
+package com.example.tradm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Home extends AppCompatActivity {
+    Button btnLogout;
+    FirebaseAuth mFirebaseaAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btnLogout = findViewById(R.id.button);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Home.this, Login.class));
+            }
+        });
+    }
+}
